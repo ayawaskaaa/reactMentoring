9d3b6f0fa6fb56fdd1e11658272ed7ce10228d38
Vasili Kazakevich <Vasili_Kazakevich@epam.com>
2020-07-26 13:20:36 +0300

    Add different types of react components

--- task1/react-components/src/App.js
+++ task1/react-components/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CreateElementExample from "./CreateElementExample";
+import ComponentExample from "./ComponentExample";
+import PureComponentExample from "./PureComponentExample";
+import FunctionalComponent from "./FunctionalComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        Hello world
+        <CreateElementExample message="Create Element"/>
+        <ComponentExample message="Component"/>
+        <PureComponentExample message="Pure component"/>
+        <FunctionalComponent message='Functional component'/>
+      </div>
   );
 }
 
---
+++ task1/react-components/src/CreateElementExample.js
@@ -0,0 +1,6 @@
+import React from 'react'
+export default class CreateElementExample extends React.Component {
+    render() {
+        return React.createElement('div', null, `Created by ${this.props.message}`)
+    }
+}
