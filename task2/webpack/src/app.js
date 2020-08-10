23af0e07b0ec58da4a9f47678eb092b9b32ae0b0
Vasili Kazakevich <Vasili_Kazakevich@epam.com>
2020-08-10 15:04:08 +0300

    Create webpack configuration

---
+++ task2/webpack/src/app.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from "react"
+
+function start(){
+    ReactDOM.render((<div>Hello world</div>), document.getElementById("app"));
+}
+start();
+
---
+++ task2/webpack/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: path.join(__dirname, "src/app.js"),
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                loader: "babel-loader",
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
